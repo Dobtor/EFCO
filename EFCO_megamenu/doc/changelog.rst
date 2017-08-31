64125faea6e936dee3bf3ee6af59aef816c4035c
Ryan Wu <ryan.wu@dobtor.com>
2017-09-01 01:56:52 +0800

    UPD

---
+++ EFCO_megamenu/doc/changelog.rst
@@ -0,0 +1,9 @@
+================================================
+``biztech_website_megamenu`` changelog
+================================================
+
+*****
+Fixed an issue where it's adding child category without assigning to megamenu
+*****
+
+Fixed an issue where it's adding child category without assigning to megamenu
---
+++ EFCO_megamenu/models/product_category.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of AppJetty. See LICENSE file for full copyright and licensing details.
+
+from openerp import api, fields, models, _
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.public.category'
+
+    include_in_megamenu = fields.Boolean(
+        string="Include in mega menu", help="Include in mega menu")
+    menu_id = fields.Many2one('website.menu', string="Main menu")
+    description = fields.Text(string="Description",
+        help="Short description which will be visible below category slider.")
