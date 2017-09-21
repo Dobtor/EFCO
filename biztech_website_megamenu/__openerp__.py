d50bc4b6bcba8511bd4242404a89a746d6eea75c
Ryan Wu <ryan.wu@dobtor.com>
2017-09-21 19:07:31 +0800

    UPD

---
+++ biztech_website_megamenu/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of AppJetty. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Advance EFCO Mega Menu',
+    'description': '''Get Smooth & Simple Site Navigation
+website mega menu
+eCommerce Menu
+eCommerce Category Menu
+multiple columns mega menu
+simple site navigation''',
+    'summary': 'Get Smooth & Simple Site Navigation',
+    'category': 'Website',
+    'version': '9.0.1.0.1',
+    'author': 'Dobtor SI,Ryan Wu',
+    'website': 'https://www.dobtor.com',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_category_view.xml',
+        'views/website_view.xml',
+        'views/template.xml',
+    ],
+    'images': ['static/description/splash-screen.png'],
+    'application': True,
+}
