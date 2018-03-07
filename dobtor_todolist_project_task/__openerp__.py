fe2d8d198a64c8e2ccadb6734fd0d3d06e1d3e80
Ryan Wu <ryan.wu@dobtor.com>
2018-03-08 05:09:17 +0800

    UPD

--- dobtor_todolist_project_task/__openerp__.py
+++ dobtor_todolist_project_task/__openerp__.py
@@ -4,7 +4,7 @@
     'version' : '1.0',
     'description' : 'Dobtor todo list project task bridge',
     'author' : "Steven, www.dobtor.com",
-    'depends' : ['base','Dobtor_TodoList_Core','project'],
+    'depends' : ['base','dobtor_todolist_core','project'],
     'data' : [
         'views/dobtor_todolist_project_task.xml',
         'data/subscription_template.xml',
