91832eec52585efc0f5b4a9872e17f8494e6d9ba
BingYiLin-Ken <key00365601@gmail.com>
2018-10-30 18:00:39 +0800

    [ADD]dobtor_efco_crm_opportunity: add dobtor_efco_crm_opportunity module

---
+++ dobtor_efco_crm_opportunity/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "dobtor_efco_crm_opportunity",
+    'summary': """
+        extend crm opportunity
+    """,
+    'description': """
+        extend crm opportunity
+    """,
+    'author': "Dobtor SI",
+    'website': "http://www.dobtor.com",
+    'category': 'crm',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'crm',
+        'product'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crm_lead_views.xml',
+        # 'views/assets.xml'
+    ],
+}
