7dd28d8579ee5bfdfe498b8cbfde3490ba786f14
kevin.zhu <kevin.zhu@dobtor.com>
2018-11-14 15:11:57 +0800

    [ADD]dobtor_efco_crm_opportunity:add expected income menu list view

---
+++ dobtor_efco_crm_opportunity/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "dobtor_efco_crm_opportunity",
+    'summary': """
+        extend crm opportunity
+    """,
+    'description': """
+        extend crm opportunity
+    """,
+    'author': "Dobtor SI",
+    'website': "http://www.dobtor.com",
+    'category': 'crm',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'crm',
+        'product'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crm_lead_views.xml',
+        'views/crm_expected_list_view.xml',
+        # 'views/assets.xml'
+    ],
+}
