d665c560e3e0cc54d4b882a4f796888bbcc5eb5d
kevin.zhu <kevin.zhu@dobtor.com>
2019-11-08 17:56:26 +0800

    [FIX] dobtor_efco_crm_opportunity:修改介面及新增end_user/opp_id

--- dobtor_efco_crm_opportunity/models/crm_lead.py
+++ dobtor_efco_crm_opportunity/models/crm_lead.py
@@ -6,11 +6,27 @@ from odoo import models, fields, api
 class crm_lead(models.Model):
     _inherit = 'crm.lead'
 
+    @api.depends('planned_revenue', 'project_lifetime')
+    def compute_project_value(self):
+        for item in self:
+            item.compute_value = item.planned_revenue * item.project_lifetime
+
+    project_lifetime = fields.Float(
+        string=u'Project lifetime', default=0.0
+    )
+    compute_value = fields.Float(
+        string=u'Compute Value', compute="compute_project_value"
+    )
     expected_income_id = fields.One2many(
         string='expected income',
         comodel_name='crm.expected.income',
         inverse_name='crm_lead_id',
     )
+    opp_id = fields.Char( string=u'Opp ID')
+
+    end_customer = fields.Many2one('res.partner', string=u"End Customer",)
+
+
 class crm_product(models.Model):
     _inherit = "product.product"
 
@@ -19,9 +35,12 @@ class crm_product(models.Model):
         res = []
         if self.env.context.get('only_product_name', False):
             for product in self.sudo():
-                variable_attributes = product.attribute_line_ids.filtered(lambda l: len(l.value_ids) > 1).mapped('attribute_id')
-                variant = product.attribute_value_ids._variant_name(variable_attributes)
-                name = variant and "%s (%s)" % (product.name, variant) or product.name
+                variable_attributes = product.attribute_line_ids.filtered(
+                    lambda l: len(l.value_ids) > 1).mapped('attribute_id')
+                variant = product.attribute_value_ids._variant_name(
+                    variable_attributes)
+                name = variant and "%s (%s)" % (
+                    product.name, variant) or product.name
                 res.append((product.id, (name)))
             return res
         return super(crm_product, self).name_get()
@@ -34,6 +53,22 @@ class crm_expected_income(models.Model):
     @api.model
     def get_current_year(self):
         return time.strftime("%Y")
+
+    @api.depends('crm_lead_id','crm_lead_id.user_id')
+    def compute_manager_region(self):
+        for item in self:
+            if item.crm_lead_id and item.crm_lead_id.user_id:
+                item.region =  item.crm_lead_id.user_id.country_id
+
+    @api.multi
+    @api.depends('q1_quantity', 'q2_quantity', 'q3_quantity', 'q4_quantity', 'amount')
+    def _compute_total_price(self):
+        for item in self:
+            item.Total_price = item.quantity * item.amount
+            item.q1_total_price = item.q1_quantity * item.amount
+            item.q2_total_price = item.q2_quantity * item.amount
+            item.q3_total_price = item.q3_quantity * item.amount
+            item.q4_total_price = item.q4_quantity * item.amount
 
     @api.model
     def get_years(self):
@@ -49,7 +84,34 @@ class crm_expected_income(models.Model):
         string='Quarterly',
         selection=[('q1', 'Q1'), ('q2', 'Q2'), ('q3', 'Q3'), ('q4', 'Q4')]
     )
-
+    q1_quantity = fields.Float(
+        string='Q1 FostQTY',
+    )
+    q2_quantity = fields.Float(
+        string='Q2 FostQTY',
+    )
+    q3_quantity = fields.Float(
+        string='Q3 FostQTY',
+    )
+    q4_quantity = fields.Float(
+        string='Q4 FostQTY',
+    )
+    q1_total_price = fields.Float(
+        string='Q1 Total',
+        compute='_compute_total_price'
+    )
+    q2_total_price = fields.Float(
+        string='Q2 Total',
+        compute='_compute_total_price'
+    )
+    q3_total_price = fields.Float(
+        string='Q3 Total',
+        compute='_compute_total_price'
+    )
+    q4_total_price = fields.Float(
+        string='Q4 Total',
+        compute='_compute_total_price'
+    )
     quantity = fields.Float(
         string='Quantity',
     )
@@ -80,51 +142,67 @@ class crm_expected_income(models.Model):
         string='Article Number',
         comodel_name="article.number"
     )
-
-    new_part_code = fields.Many2one(
-        string="Part Number",
-        comodel_name="part.number")
-
-    @api.multi
-    @api.depends('quantity','amount')
-    def _compute_total_price(self):
-        for item in self:
-            item.Total_price = item.quantity * item.amount
+    partner_id  = fields.Many2one(string=u'Revenue Customer',related="crm_lead_id.partner_id")
+    opp_id = fields.Char(related="crm_lead_id.opp_id")
+    end_customer = fields.Many2one(related="crm_lead_id.end_customer")
+    new_part_code = fields.Many2one(string="Part Number", comodel_name="part.number")
+    stage_id  = fields.Many2one(
+       related="crm_lead_id.stage_id"
+    )
+    region = fields.Many2one('res.country', string='Region',compute="compute_manager_region")
+    business_line = fields.Many2one(string=u'BL',related="crm_lead_id.team_id")
 
     @api.onchange('article_number')
     def do_change_product(self):
-        if self.article_number:
-            article_number = self.article_number
-            product = self.env['product.product'].search([('new_part_code','=',article_number.id)],limit=1)
-            if not self.product_id and product:
-                self.product_id = product
-            if self.product_id.new_part_code!=self.article_number and product:
-                self.product_id = product
-            if self.product_id.new_part_code!=self.article_number and not product:
-                self.product_id = False
+        for record in self:
+            if record.article_number:
+                product = self.env['product.product'].search(
+                    [('new_part_code', '=', record.article_number.id)], limit=1)
+                if not record.product_id and product:
+                    record.product_id = product
+                elif record.product_id and record.product_id.new_part_code != record.article_number and product:
+                    record.product_id = product
+                elif record.product_id and record.product_id.new_part_code != record.article_number and not product:
+                    record.product_id = False
+
+                # if not record.new_part_code and product and product.old_part_code:
+                #     record.new_part_code = product.old_part_code
+                # elif record.new_part_code and product and product.old_part_code and record.new_part_code != product.old_part_code:
+                #     record.new_part_code = product.old_part_code
+
     @api.onchange('product_id')
     def do_change_article_number(self):
-        if self.product_id:
-            product = self.product_id
-            if not self.article_number and product.new_part_code:
-                self.article_number = product.new_part_code
-            if self.article_number and product.new_part_code and  self.article_number != product.new_part_code :
-                self.article_number = product.new_part_code
+        for record in self:
+            if record.product_id and record.product_id.new_part_code:
+                if not record.article_number:
+                    record.article_number = record.product_id.new_part_code
+                elif record.article_number and record.article_number != record.product_id.new_part_code:
+                    record.article_number = record.product_id.new_part_code
+            else:
+                record.article_number = False
 
-            if not self.new_part_code and product.old_part_code:
-                self.new_part_code = product.old_part_code
-            if self.new_part_code and product.old_part_code and  self.new_part_code != product.old_part_code :
-                self.new_part_code = product.old_part_code
-
+            if record.product_id and record.product_id.old_part_code:
+                if not record.new_part_code:
+                    record.new_part_code = record.product_id.old_part_code
+                elif record.product_id.old_part_code and record.new_part_code != record.product_id.old_part_code:
+                    record.new_part_code = record.product_id.old_part_code
+            else:
+                record.new_part_code = False
 
     @api.onchange('new_part_code')
     def do_product_by_new_part_code(self):
-        if self.new_part_code:
-            new_part_code = self.new_part_code
-            product = self.env['product.product'].search([('old_part_code','=',new_part_code.id)],limit=1)
-            if not self.product_id and product:
-                self.product_id = product
-            if self.product_id.old_part_code  !=self.new_part_code and product:
-                self.product_id = product
-            if self.product_id.old_part_code  !=self.new_part_code and not product:
-                self.product_id = False
+        for record in self:
+            if record.new_part_code:
+                product = self.env['product.product'].search(
+                    [('old_part_code', '=', record.new_part_code.id)], limit=1)
+                if not record.product_id and product:
+                    record.product_id = product
+                elif record.product_id and product and record.product_id.old_part_code != record.new_part_code:
+                    record.product_id = product
+                elif record.product_id and not product and record.product_id.old_part_code != record.new_part_code:
+                    record.product_id = False
+
+                # if not record.article_number and product and product.new_part_code:
+                #     record.article_number = product.new_part_code
+                # elif record.article_number and record.article_number != product.new_part_code and product and product.new_part_code:
+                #     record.article_number = product.new_part_code
